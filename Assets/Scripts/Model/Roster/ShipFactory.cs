e979e33e503493cc4479901d86aaaac6933d30b7
sampson-matt <sampson.matt@gmail.com>
2025-03-07 13:40:07 -0600

    remove 2.5 range 0 dice mods rule

--- Assets/Scripts/Model/Roster/ShipFactory.cs
+++ Assets/Scripts/Model/Roster/ShipFactory.cs
@@ -60,10 +60,6 @@ public static class ShipFactory {
         newShipContainer.AfterAssignedDamageIsChanged += Roster.UpdateRosterHullDamageIndicators;
         newShipContainer.AfterAssignedDamageIsChanged += Roster.UpdateRosterShieldsDamageIndicators;
         newShipContainer.AfterStatsAreChanged += Roster.UpdateShipStats;
-        newShipContainer.OnTryAddAvailableDiceModification += Rules.DiceModification.PreventRangeZeroOwnModifications;
-        newShipContainer.OnTryAddDiceModificationAfterRolled += Rules.DiceModification.PreventRangeZeroOwnModifications;
-        newShipContainer.OnTryAddDiceModificationCompareResults += Rules.DiceModification.PreventRangeZeroCompareResultsModifications;
-        newShipContainer.OnTryAddDiceModificationOpposite += Rules.DiceModification.PreventRangeZeroOwnModifications;
 
         Edition.Current.SubScribeToGenericShipEvents(newShipContainer);
 
--- Assets/Scripts/Model/Rules/Rules.cs
+++ Assets/Scripts/Model/Rules/Rules.cs
@@ -37,7 +37,6 @@ public static class Rules
     public static RemotesRule Remotes { get; private set; }
     public static FuseRule Fuse { get; private set; }
     public static PurpleManeuversRule PurpleManeuvers { get; private set; }
-    public static DiceModificationRule DiceModification { get; private set; }
 
 
     public static void Initialize()
@@ -79,7 +78,6 @@ public static class Rules
         Fuse = new FuseRule();
         Remotes = new RemotesRule();
         PurpleManeuvers = new PurpleManeuversRule();
-        DiceModification = new DiceModificationRule();
     }
 
     public static void FinishGame()
